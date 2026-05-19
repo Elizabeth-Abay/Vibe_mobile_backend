bc44375a5c0dd73238d567f06b3b2a1d920f8299
Elizabeth <ElizabethAbayZewdie@outlook.com>
2026-05-19 14:13:48 +0300

    Interest model first function

---
+++ config/neo4jConfig.js
@@ -0,0 +1,39 @@
+const neo4j = require('neo4j-driver');
+
+const dotenv = require('dotenv');
+
+const path = require('path');
+
+
+dotenv.config(
+    {
+        path: path.resolve(__dirname, '../../.env')
+    }
+)
+
+let { Neo4jPassword, Neo4jConnectionURI, Neo4jUser } = process.env;
+
+
+// here we will create a driver and export it for others to use it
+let driver = neo4j.driver(
+    Neo4jConnectionURI,
+    neo4j.auth.basic(
+        Neo4jUser,
+        Neo4jPassword
+    )
+);
+
+async function isConnected() {
+    try {
+        return await driver.verifyConnectivity();
+    } catch (err) {
+        // if it is not throw an error
+        console.log("Error while connecting with the graph ", err);
+    }
+
+}
+
+isConnected();
+
+
+module.exports = driver;
--- model/interestModelGraph.js
+++ model/interestModelGraph.js
@@ -1,46 +1,125 @@
-async linkingInterest(sentInfo) {
-    try {
-        // linking
-        // sentInfo = { userId , interestedIn : { name : value }}
-        // convert that to interests : [ { name : something , value : Number(value)}]
+class InterestModelG {
+    async linkingUserWithInterests(sentInfo) {
+        try {
+            // query - interests = [ { name , rated_as }]
+            // then u will have it unwind - ie separate the object 
+            // interest - will be the current item
+            const query = `
+                MATCH (n:Person {id: $id})
+                UNWIND $interests AS interest
+                MATCH (i:Interest {name: interest.name})
+                MERGE (n)-[r:INTERESTED_IN ]->(i)
+                SET r.rated_as = interest.rated_as
+                RETURN n, collect(r) AS relationships
+            `;
+            // since interests is an array of objects
+            // unwind will run the query below it once for every object termed as interest
 
-        let { userId, interestedIn } = sentInfo;
 
-        let Interest = [];
+            let result = await sessionToWrite.executeWrite(
+                tx => {
+                    return tx.run(
+                        query, sentInfo
+                    )
+                }
 
-        for (let interest in interestedIn) {
-            // interest is the key;
-            let key = interest;
-            let val = interestedIn[interest]
+            )
 
-            let Obj = {
-                name: key,
-                value: Number(val)
-            }
 
-            Interest.push(Obj)
-        }
+            return (result.records[0]?.length === 0) ? { success: false } : { success: true }
+           
 
-        let result = await signUpGraph.linkingUserWithInterests({ userId, Interest });
+        } catch (err) {
+            console.log("Error while GraphActivities.linkingUserWithInterests ", err.message);
 
-        if (result.success) {
             return {
-                success: true
+                success: false,
+                reason: "Error while GraphActivities.linkingUserWithInterests"
             }
-        }
-
-        return {
-            success: false,
-            reason: "data base problem from linkingInterest"
-
-        }
-    } catch (err) {
-        console.log("Error while  linkingInterest", err.message);
-
-        return {
-            success: false,
-            reason: "Error while  linkingInterest"
         }
     }
 
+    async updatingLinksOfInterests(sentInfo) {
+        try {
+            // sentInfo = { userId , interests : [ { name , rated_as }]}
+            // first delete all the links
+            // then recreate them
+
+            let queryToDel = `
+            MATCH (n:Person) WHERE n.userId = $userId
+            MATCH (n)-[r:INTERESTED_IN]->()
+            DELETE r
+            RETURN n
+            `
+
+            let delRes = await sessionToWrite.executeWrite(
+                tx => {
+                    return tx.run(
+                        queryToDel, sentInfo
+                    )
+
+                }
+            )
+
+            if (delRes.records[0]?.length === 0) {
+                return {
+                    success: false,
+                    reason: "Database issue while deleting old interest links"
+                }
+            }
+
+            let res = this.linkingUserWithInterests(sentInfo);
+            // recreate interest links
+
+            return res;
+
+        } catch (err) {
+            console.log("Error while GraphActivities.updatingLinksOfInterests ", err.message);
+            return {
+                success: false,
+                reason: "Error while GraphActivities.updatingLinksOfInterests"
+            }
+
+
+
+        }
+    }
+
+    async gettingAllInterest(sentInfo) {
+        try {
+            let { userId } = sentInfo;
+            // we want to get all names of nodes which are linked by interested_in
+            let query = `
+            MATCH (u:Person) WHERE u.userId = $userId
+            MATCH (u)-[r:INTERESTED_IN]->(i)
+            RETURN collect(i.name) , collect(r.rated_as)
+            `
+
+            let res = await sessionToRead.executeRead(
+                tx => {
+                    return tx.run(
+                        query, { userId }
+                    )
+                }
+            )
+            // res.records[0]._fields this is an array made up of 2 arrays 
+            // the front end only needs the [{ name , rated_as}]
+            // so loop through both arrays in the service layer  
+
+            return {
+                success: true,
+                data: res.records[0]._fields
+            }
+
+        } catch (err) {
+            console.log("Error while GraphActivities.gettingAllInterest ", err.message);
+            return {
+                success: false,
+                reason: "Error while GraphActivities.gettingAllInterest"
+            }
+
+
+
+        }
+    }
 }
